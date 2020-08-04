083ad448f3b2e1b6c9c435efa1da9a9e700055ac
Nayla Saenz Lopez <naylasaenz@gmail.com>
2020-08-04 12:25:45 -0300

    ejercicio nueve corregido

--- 09-exercise/src/ar/com/ada/online/second/exercisenine/Main.java
+++ 09-exercise/src/ar/com/ada/online/second/exercisenine/Main.java
@@ -9,8 +9,7 @@ public class Main {
         // Una compañía de seguros para autos ofrece dos tipos de póliza: cobertura amplia (A) y
         //daños a terceros (B).
         // Para el plan A, la cuota base es de $1,200, y para el B, de $950.
-        // A ambos planes se les carga 10% del costo si la persona que conduce tiene por hábito
-        //beber alcohol
+        // A ambos planes se les carga 10% del costo si la persona que conduce tiene por hábito beber alcohol
         // 5% si utiliza lentes,
         // 5% si padece alguna enfermedad –como deficiencia cardiaca o diabetes–,
         // y si tiene más de 40 años, se le carga 20%,
